bdfe1c6904e662296b14a9d770967ff667443405
Liz Baldelomar <hello@lizethb.com>
2023-09-12 18:25:34 +0800

    Write results to HTML instead of console

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const results = document.querySelector(".results-output");
+
 function getComputerChoice() {
   const choices = ['ROCK', 'PAPER', 'SCISSORS'];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -5,19 +7,17 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log(`Player: ${playerSelection}. Computer: ${computerSelection}. Draw!`);
+    results.textContent += `Player: ${playerSelection}. Computer: ${computerSelection}. Draw!\n`;
     return 0;
-  }
-  else if (
+  } else if (
     (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
     (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
     (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
     ) {
-    console.log(`Player: ${playerSelection}. Computer: ${computerSelection}. You win!`);
+    results.textContent += `Player: ${playerSelection}. Computer: ${computerSelection}. You win!\n`;
     return 1;
-  }
-  else {
-    console.log(`Player: ${playerSelection}. Computer: ${computerSelection}. You lose!`);
+  } else {
+    results.textContent += `Player: ${playerSelection}. Computer: ${computerSelection}. You lose!\n`;
     return -1;
   }
 }
@@ -25,36 +25,31 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   let playerScore = 0;
   let computerScore = 0;
-
   while (playerScore < 5 && computerScore < 5) {
     const playerInput = prompt("Rock, Paper, or Scissors?").toUpperCase();
     if (playerInput === 'ROCK' || playerInput === 'PAPER' || playerInput === 'SCISSORS') {
       const roundResult = playRound(playerInput, getComputerChoice());
       if (roundResult === 1) {
         playerScore++;
-      }
-      else if (roundResult === -1) {
+      } else if (roundResult === -1) {
         computerScore++;
       }
-      console.log(`${playerScore}-${computerScore}`);
-    }
-    else {
-      console.log("Invalid input");
+      results.textContent += `${playerScore}-${computerScore}\n\n`;
+    } else {
+      results.textContent += "Invalid input\n\n";
     }
   }
   
   let gameResult;
   if (playerScore > computerScore) {
     gameResult = "Player";
-  }
-  else if (computerScore > playerScore) {
-    gameResult = "Computer"
-  }
-  else if (playerScore === computerScore) {
-    gameResult = "Draw"
+  } else if (computerScore > playerScore) {
+    gameResult = "Computer";
+  } else if (playerScore === computerScore) {
+    gameResult = "Draw";
   }
 
-  console.log(`Winner: ${gameResult}`)
+  results.textContent += `Winner: ${gameResult}\n`;
 }
 
 game();
